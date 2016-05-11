6773c03661be613c18193314b8e9dfbd0d27436e
bzvyagintsev <b.zvyagintsev@gmail.com>
2016-05-11 23:17:25 +0300

    update

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,16 +1,2 @@
 module BooksHelper
-	def current_issuances array
-		count = 0
-		array.each do |item|
-			if item.issuances.current
-				item.issuances.current.each do |issuance|
-					count = count + 1
-				end
-			end
-		end
-
-		return count
-	end
-
-	
 end
